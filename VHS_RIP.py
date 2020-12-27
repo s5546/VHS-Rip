f614d8b596aefdb9dc1aa24cb2d3c22cc1338ce4
Cassie <spencerfinch7@gmail.com>
2020-12-27 16:09:30 -0500

    how does github work

--- VHS_RIP.py
+++ VHS_RIP.py
@@ -6,14 +6,6 @@ import time
 import tkinter as tk
 import datetime
 import threading
-from playsound import playsound
-## MAIN
-# ask user for the movie's name
-# ask user how long movie is
-# internally save the length variable as seconds (for audio) and hh:mm:ss (for video)
-# ask user to rewind their tape, pause at beginning of tape, and to get ready to hit play when the console prints "START"
-# Countdown: 5, 4, 3, 2, 1, START
-# SLEEP FOR TWO SECONDS
 class VHSGui:
     root = tk.Tk()
     time_seconds = 0
@@ -21,23 +13,22 @@ class VHSGui:
     recording = False
     audio_thread = None
     video_thread = None
+    endtime_audio_thread = None
+    endtime_video_thread = None
     ffmpeg_thread = None
     start_time = None
     file_name = ""
     label_HHMMSS = tk.StringVar(value=time_HHMMSS)
     butt_recordStatus = tk.StringVar(value="Push me to start...")
-    len_entry_id = None
-    name_entry_id = None
 
     def __init__(self):
         self.root.geometry("640x480")
         self.root.title=("Cassie's VHS Ripper")
         self.root.remainingLabel = tk.Label(textvariable=self.label_HHMMSS).pack() # its HERE make this an actual StringVar
-        self.root.entryLabel = tk.Label(text="Please type the name of the file you want to save to.").pack()
-        self.root.timeEntry = tk.Entry(self.root, width=80).pack()
+        self.root.nameLabel = tk.Label(text="Please type the name of the file you want to save to.").pack()
+        self.root.nameEntry = tk.Entry(self.root, width=80).pack()
         self.root.timeEntryLabel = tk.Label(text="Please type the duration of your VHS Tape, in HH:MM:SS format or in seconds").pack()
         self.root.timeEntry = tk.Entry(self.root, width=8).pack()
-        self.len_entry_id = id(self.root.getvar())
         self.root.startRecordButt = tk.Button(textvariable=self.butt_recordStatus, command=self.start_recording).pack()
         self.root.endRecordButt = tk.Button(text="Click me to end recording early", command=self.early_end_recording).pack()
         self.root.timeWarningLabel = tk.Label(text="WARNING: The runtime reported on most VHS tapes is of the movie ONLY!\n" +
@@ -51,15 +42,18 @@ class VHSGui:
         if count == 4:
             tempChildren = self.root.winfo_children()
             for i in range(len(tempChildren)):
+                # im so sorry
                 if str(type(tempChildren[i])) == '<class \'tkinter.Entry\'>': # there has to be a better way
-                    temp_entry_value = tempChildren[i].get()
-                    if (":" in temp_entry_value):
-                        self.time_seconds = self.HHMMSS_to_seconds(temp_entry_value)
-                        self.time_HHMMSS = temp_entry_value
+                    if tempChildren[i].winfo_width() > 400: # totally breaks if window gets scaled
+                        self.file_name = tempChildren[i].get()
                     else:
-                        self.time_seconds = int(temp_entry_value)
-                        self.time_HHMMSS = self.seconds_to_HHMMSS(int(temp_entry_value))
-                    break
+                        temp_entry_value = tempChildren[i].get()
+                        if (":" in temp_entry_value):
+                            self.time_seconds = self.HHMMSS_to_seconds(temp_entry_value)
+                            self.time_HHMMSS = temp_entry_value
+                        else:
+                            self.time_seconds = int(temp_entry_value)
+                            self.time_HHMMSS = self.seconds_to_HHMMSS(int(temp_entry_value))
             self.recording = True
             self.butt_recordStatus.set("Get ready!")
             self.start_threads()
@@ -67,6 +61,9 @@ class VHSGui:
         elif count == 0:
             self.updateTime()
             self.butt_recordStatus.set("RECORDING")
+            print(self.file_name)
+            print(self.time_HHMMSS)
+            print(self.time_seconds)
         else:
             self.butt_recordStatus.set(count)
             self.root.after(1000, self.countdown, count-1)
@@ -85,28 +82,34 @@ class VHSGui:
                 elif not mins == 0: # this is a catchall, but one that ignores hour boundries (which an else statement wouldn't've)
                     print("oopsie we made a fucky wucky")
         hrs = str(hrs)
+        mins = str(mins)
+        seconds = str(seconds)
         if len(hrs) == 1:
             hrs = "0" + hrs
-        mins = str(mins)
         if len(mins) == 1:
             mins = "0" + mins
-        seconds = str(seconds)
+        if len(seconds) == 1:
+            seconds = "0" + seconds
         # todo: convert single digits to double digits
         return str(hrs) + ":" + str(mins) + ":" + str(seconds) 
 
     def HHMMSS_to_seconds(self, HHMMSS): # lots of division, be careful
         hms_array = HHMMSS.split(":")
-        # todo: see if this section breaks if hours > 24
-        temp = int(hms_array[0]) * 3600 + \
-                int(hms_array[1]) * 60 + \
-                    int(hms_array[2])
-        print(type(temp))
-        return temp
+        hms_array = HHMMSS.split(":")
+        if (len(hms_array) == 2): # if its MM:SS
+            hms_array.insert(0, "00")
+        # todo: this breaks if the user enters a day (DD:HH:MM:SS)
+        temp_range = len(hms_array)
+        temp_seconds = 0
+        for i in range(temp_range):
+            temp_seconds = temp_seconds + (int(hms_array[i]) * (60 ** (temp_range - 1 - i)))
+        return temp_seconds
 
     def early_end_recording(self):
         self.time_seconds = 1
 
     def start_threads(self):
+        # todo: get sudo permissions before starting this
         self.start_time = time.ctime()
         print(self.start_time)
         self.audio_thread = threading.Thread(target=self.record_audio)
@@ -119,11 +122,15 @@ class VHSGui:
         self.audio_thread = subprocess.Popen(["sudo arecord --device=\"hw:CARD=Bt878,DEV=0\" tempaudio.wav -f cd -N -d " + str(self.time_seconds)], shell=True)
         print("aud", self.audio_thread.pid)
         self.audio_thread.wait()
+        self.endtime_audio_thread = time.time()
+        
 
     def record_video(self): 
         self.video_thread = subprocess.Popen(["sudo streamer -p 4 -r 24 -q -o tempvideo.avi -j 90 -f mjpeg -n ntsc -t " + str(self.time_HHMMSS)], shell=True)
         print("vid", self.video_thread.pid)
         self.video_thread.wait()
+        self.endtime_video_thread = time.time()
+        
 
     def updateTime(self):
         if self.recording == True:
@@ -140,8 +147,18 @@ class VHSGui:
                 self.video_thread.send_signal(15)
                 print("aud", self.audio_thread.returncode)
                 print("vid", self.video_thread.returncode)
-                self.audio_thread
-                self.ffmpeg_thread = subprocess.Popen(["sudo ffmpeg -i tempvideo.avi -i tempaudio.wav -c copy -movflags use_metadata_tags -map 0:v -map 1:a test.mkv"], shell=True)
+
+                diff = self.endtime_video_thread - self.endtime_audio_thread
+                if diff > 0: # if the audio started later than the video
+                 self.ffmpeg_thread = subprocess.Popen(["sudo ffmpeg -hwaccel cuda -i tempaudio.wav -itsoffset " + str(diff) \
+                       + " -i tempvideo.avi -c:v libx265 -c copy -movflags use_metadata_tags -map 1:v -map 0:a " + \
+                           self.file_name + ".mkv"], shell=True) # technically, the float -> str casting makes us lose precision, but only by hundredthousandths of a second
+                elif diff < 0: # if the video started later than the audio
+                 self.ffmpeg_thread = subprocess.Popen(["sudo ffmpeg -hwaccel cuda -i tempvideo.avi -itsoffset " + str(diff) \
+                     + " -i tempaudio.wav -c:v libx265 -c copy -movflags use_metadata_tags -map 0:v -map 1:a " + \
+                          self.file_name + ".mkv"], shell=True)
+                else: # seems unlikely but ill go with it
+                    self.ffmpeg_thread = subprocess.Popen(["sudo ffmpeg -i tempvideo.avi -i tempaudio.wav -c copy -movflags use_metadata_tags -map 0:v -map 1:a " + self.file_name + ".mkv"], shell=True)
 
 
 test = VHSGui()
